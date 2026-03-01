a36e9e52722527ec90ccd7b1af9f3e831d6b860e
botikal <117993676+botikal@users.noreply.github.com>
2026-03-01 20:56:05 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 203 ms, Memory: 87,680 KB -BaekjoonHub

---
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
@@ -0,0 +1,12 @@
+T = int(input())
+for t in range(1, T+1):
+    word_a, word_b = map(list, input().split())
+    len_a, len_b = len(word_a), len(word_b)
+    dp = [[0] * (len_b+1) for _ in range(len_a+1)]
+    for i in range(1, len_a+1):
+        for j in range(1, len_b+1):
+            if word_a[i-1] == word_b[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    print(f'#{t} {dp[len_a][len_b]}')
